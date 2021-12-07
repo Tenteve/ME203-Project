b39f03d6936094280b0644b03075856467042a9b
Asimone47 <94400860+Asimone47@users.noreply.github.com>
2021-12-06 18:02:18 -0800

    Update ME203_Project_P1.m
    Added another set of while loops for checking if a space is taken or not, the game works as intended now.

--- Project Tasks/ME203_Project_P1.m
+++ Project Tasks/ME203_Project_P1.m
@@ -12,49 +12,59 @@ plot([2.5 2.5],[-.5 -3.5],'k','linewidth',2)
 hold off
 axis off
 
-%Whoever makes the first move is player 1
-move=input('Player 1 [r,c]:');
-r=move(1);
-c=move(2);
-board(r,c)=1;
-text(c,-r,'X','horizontalalignment','center','fontsize',25)
-turn=1;
+%Whoever makes the first move is player 1 (X)
+move=input('Player 1 [r,c]:'); % input for move by player 1
+r=move(1); % row position
+c=move(2); % column position
+board(r,c)=1; % logical value of player 1
+text(c,-r,'X','horizontalalignment','center','fontsize',25) % displaying the (X)
+turn=1; % this is to initialize the while loops, they begin after the first turn or turn==1
 %Begins the loop with player 2's first turn
 while turn==1
-    move=input('Player 2 [r,c]:');
+    move=input('Player 2 [r,c]:'); % Player 2's move
     r=move(1);
     c=move(2);
-    while board(r,c)==abs(1)   %While loop Checks if move is already taken
-        move=input('Invalid input Player 2 [r,c]:');
+    while board(r,c)==1   %While loop Checks if move is already taken by player 1
+        move=input('Invalid input Player 2 [r,c]:'); % If move is taken, try again
         r=move(1);
         c=move(2);
     end
-    board(r,c)=-1;
-    text(c,-r,'O','horizontalalignment','center','fontsize',25)
+        while board(r,c)==-1    %While loops Checks if move is already taken by player 2
+          move=input('Invalid input Player 1 [r,c]:'); % If move is taken, try again
+          r=move(1);
+          c=move(2);
+        end
+    board(r,c)=-1; % logical value of player 2
+    text(c,-r,'O','horizontalalignment','center','fontsize',25) % player 2 is O
       result=[sum(board),sum(board)]; %These are the winning scenarios
-      result(1)=board(1,1)+board(1,2)+board(1,3); 
+      result(1)=board(1,1)+board(1,2)+board(1,3); % any consecutive 3 in a row
       result(2)=board(2,1)+board(2,2)+board(2,3);
       result(3)=board(3,1)+board(3,2)+board(3,3);
-      result(4)=board(1,1)+board(2,1)+board(3,1);
+      result(4)=board(1,1)+board(2,1)+board(3,1); % any consecutive 3 in a column
       result(5)=board(1,2)+board(2,2)+board(3,2);
       result(6)=board(1,3)+board(2,3)+board(3,3);
-      result(7)=board(1,1)+board(2,2)+board(3,3);
+      result(7)=board(1,1)+board(2,2)+board(3,3); % any consecutive 3 along a diagonal
       result(8)=board(1,3)+board(2,2)+board(3,1);
-      if any(result==-3) %Checks for a win by player 2 and ends loop with a win
-          disp('Player 2 wins')
-          break
+      if any(result==-3) % If player 2 gets any 3 consecutive spaces, they win
+          disp('Player 2 wins') % Player 2 wins 
+          break % ends loop if player 2 wins
       end
       move=input('Player 1 [r,c]:'); %Now player 1's turn is in the loop 
       r=move(1);
       c=move(2);
-      while board(r,c)==abs(1)    %While loops Checks if move is already taken
-          move=input('Invalid input Player 2 [r,c]:');
+      while board(r,c)==-1    %While loops Checks if move is already taken by player 2
+          move=input('Invalid input Player 1 [r,c]:');
           r=move(1);
           c=move(2);
       end
+        while board(r,c)==1    %While loops Checks if move is already taken by player 1
+          move=input('Invalid input Player 1 [r,c]:');
+          r=move(1);
+          c=move(2);
+        end
       board(r,c)=1;
       text(c,-r,'X','horizontalalignment','center','fontsize',25)
-      result=[sum(board),sum(board)];  %These are the winning scenarios
+      result=[sum(board),sum(board)];  %These are the same winning scenarios
       result(1)=board(1,1)+board(1,2)+board(1,3);
       result(2)=board(2,1)+board(2,2)+board(2,3);
       result(3)=board(3,1)+board(3,2)+board(3,3);
@@ -68,11 +78,10 @@ while turn==1
           break
       end
         if all(result~=3)&(sum(sum(abs(board)))==9) %Checks for a tie
-        disp('Tie game')
+        disp('Tie game') % If all board spaces fill up then the game ends in a tie
         break
         end
  end
-
 
 
 
