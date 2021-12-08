f81b3e705a197e4a31bdf8411c9ce586d24c335f
CaseyJ2019 <95445050+CaseyJ2019@users.noreply.github.com>
2021-12-07 23:11:13 -0800

    Update ME203_Project_P3.m

--- Project Tasks/Project3_Avengers/ME203_Project_P3.m
+++ Project Tasks/Project3_Avengers/ME203_Project_P3.m
@@ -6,3 +6,27 @@ Output=remove_delim(data); %removes .,!? and lowercases it
 [data_filtered,L] = clean_words (Output);
 [barx,count]=categorizes_words(data_filtered);
 [TOP,subcount]=Top_N (count,barx,1) %this will print more than N amount of top words if it exeeds the amount that there are.
+function [TOP,subcount]=Top_N (count,barx,N)
+
+TOP_N=maxk(count,N);
+i=0;
+while i<length(TOP_N)
+    i=i+1;
+    f=find(count==TOP_N(i));
+    if length(f)>1
+        for j=1:length(f)
+            g(i)=f(j); 
+            i=i+1;
+            if j==length(f)
+                i=i-1;
+            end
+            
+        end
+    else
+        g(i)=f;
+    end
+end
+TOP=barx(g);
+for c=1:length(g)
+    subcount(c)=count(g(c));
+end
