a79ee65390bc887322434a1321111aedccb81243
Tenteve <94567685+Tenteve@users.noreply.github.com>
2021-12-08 20:50:30 -0800

    Add files via upload

--- Project Tasks/Project3_Avengers/ME203_Project_P3.m
+++ Project Tasks/Project3_Avengers/ME203_Project_P3.m
@@ -1,7 +1,11 @@
 clc; clear all; format compact;
 
+% User input for Text and N indicating their desired hero and WordCount
+Text = append(input('Enter hero script \n Use underscore for spaces(Ex:Iron_Man)\n Try (Avenger_Assemble) for all the Avengers: ','s'),'.txt');
 
-Text = append(input('Enter hero script \n Use underscore for spaces(Ex:Iron_Man): ','s'),'.txt');
+c=["Steve_Rogers.txt" "Tony_Stark.txt" "Thor.txt" "Bruce_Banner.txt" "Natasha_Romanoff.txt" "Clint_Barton.txt"]; %made specifcally for Avengers assemble for a simple for loop
+
+N = input('How many top words would you like to see? (1,2,3...): ');
 
 switch Text
     case 'Captain_America.txt'
@@ -16,9 +20,17 @@ switch Text
         Text='Natasha_Romanoff.txt'
     case 'Hawkeye.txt'
         Text='Clint_Barton.txt'
+    case 'Avenger_Assemble.txt'
+        for i=1:6
+            [AFTop,AFCount,AFL(i)] = AdvFilter(c(i),N); %Referback to Line6
+            subplot(2,3,i)
+            bar(categorical(AFTop),AFCount);
+            title(c(i),'Interpreter','none') %The extra interpreter-none makes it so that the Underscore doesnt make it subscript
+            xlabel('Top Words')
+            ylabel('Count')
+        end
+        return %Since this case already complete all of the Avengers, this ends the script
 end
-
-N = input('How many top words would you like to see? (1,2,3...): ');
 
 [AFTop,AFCount,AFL] = AdvFilter(Text,N)
 
