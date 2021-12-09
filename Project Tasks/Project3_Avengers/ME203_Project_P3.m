53be2f2d03afa30f68742b8aa915b39551cb9a11
Tenteve <94567685+Tenteve@users.noreply.github.com>
2021-12-08 19:57:41 -0800

    Add files via upload

--- Project Tasks/Project3_Avengers/ME203_Project_P3.m
+++ Project Tasks/Project3_Avengers/ME203_Project_P3.m
@@ -1,32 +1,27 @@
 clc; clear all; format compact;
 
-%% this is a check to see if every function works
-data=get_words('Thor.txt'); %must be a string .txt 
-Output=remove_delim(data); %removes .,!? and lowercases it
-[data_filtered,L] = clean_words (Output);
-[barx,count]=categorizes_words(data_filtered);
-[TOP,subcount]=Top_N (count,barx,1) %this will print more than N amount of top words if it exeeds the amount that there are.
-function [TOP,subcount]=Top_N (count,barx,N)
 
-TOP_N=maxk(count,N);
-i=0;
-while i<length(TOP_N)
-    i=i+1;
-    f=find(count==TOP_N(i));
-    if length(f)>1
-        for j=1:length(f)
-            g(i)=f(j); 
-            i=i+1;
-            if j==length(f)
-                i=i-1;
-            end
-            
-        end
-    else
-        g(i)=f;
-    end
+Text = append(input('Enter hero script \n Use underscore for spaces(Ex:Iron_Man): ','s'),'.txt');
+
+switch Text
+    case 'Captain_America.txt'
+        Text='Steve_Rogers.txt'
+    case 'Iron_Man.txt'
+        Text='Tony_Stark.txt'
+    case 'Thor_Odinson.txt'
+        Text='Thor.txt'
+    case 'Hulk.txt'
+        Text='Bruce_Banner.txt'
+    case 'Black_Widow.txt'
+        Text='Natasha_Romanoff.txt'
+    case 'Hawkeye.txt'
+        Text='Clint_Barton.txt'
 end
-TOP=barx(g);
-for c=1:length(g)
-    subcount(c)=count(g(c));
-end
+
+N = input('How many top words would you like to see? (1,2,3...): ');
+
+[AFTop,AFCount,AFL] = AdvFilter(Text,N)
+
+bar(categorical(AFTop),AFCount);
+xlabel('Top Words')
+ylabel('Count')
